5b1ba15e2f63631ac8d89b4b63a9d60d0ba21caa
haase <haase@6d381ed0-77be-451f-88c9-2c4333ae1e5d>
2009-04-22 22:42:32 +0000

    Extended setops implementation

--- setops.js
+++ setops.js
@@ -17,6 +17,13 @@ function _fdjt_set_sortfn(a,b)
   else return 1;
 }
 
+function _fdjt_length_sortfn(a,b)
+{
+  if (a.length===b.length) return 0;
+  else if (a.length<b.length) return -1;
+  else return 1;
+}
+
 function fdjtSet(arg,destructive)
 {
   if (!(arg)) return new Array();
@@ -25,6 +32,7 @@ function fdjtSet(arg,destructive)
     else if ((arg._sortlen) && ((arg._sortlen) === (arg.length)))
       return arg;
     else {
+      if (!(destructive)) arg=arg.slice(0);
       arg.sort(_fdjt_set_sortfn);
       var read=1; var write=1; var len=arg.length;
       var cur=arg[0];
@@ -35,4 +43,95 @@ function fdjtSet(arg,destructive)
       arg.length=write;
       return arg;}
   else return new Array(arg);
+}
+
+function fdjt_intersect(set1,set2)
+{
+  var results=new Array();
+  var i=0; var j=0; var len1=set1.length; var len2=set2.length;
+  while ((i<len1) && (j<len2))
+    if (set1[i]===set2[j]) {
+      results.push(set1[i]); i++; j++;}
+    else if (_fdjt_set_sortfn(set1[i],set2[j])<0) i++;
+    else j++;
+  return results;
+}
+
+function fdjtIntersect()
+{
+  if (arguments.length===0) return new Array();
+  else if (arguments.length===1)
+    return fdjtSet(arguments[0],true);
+  else if (arguments.length===2)
+    return fdjt_intersect(fdjtSet(arguments[0],true),
+			  fdjtSet(arguments[1],true));
+  else {
+    var i=0; while (i<arguments.length)
+	       if (!(arguments[i])) return new Array();
+	       else if ((typeof arguments[i] === "object") &&
+			(arguments[i] instanceof Array) &&
+			(arguments[i].length===0))
+		 return new Array();
+	       else i++;
+    var copied=arguments.slice(0);
+    copied.sort(fdjt_len_sortfn);
+    var results=fdjtSet(copied[0],true);
+    i=1; while (i<copied.length) {
+      results=fdjt_intersect(results,fdjtSet(copied[i++],true));
+      if (results.length===0) return results;}
+    return results;}
+}
+
+function fdjt_union(set1,set2)
+{
+  var results=new Array();
+  var i=0; var j=0; var len1=set1.length; var len2=set2.length;
+  while ((i<len1) && (j<len2))
+    if (set1[i]===set2[j]) {
+      results.push(set1[i]); i++; j++;}
+    else if (_fdjt_set_sortfn(set1[i],set2[j])<0)
+      results.push(set1[i++]);
+    else results.push(set2[j++]);
+  while (i<len1) results.push(set1[i++]);
+  while (j<len2) results.push(set2[j++]);
+  return results;
+}
+
+function fdjtUnion()
+{
+  if (arguments.length===0) return new Array();
+  else if (arguments.length===1) return fdjtSet(arguments[0]);
+  else if (arguments.length===2)
+    return fdjt_union(fdjtSet(arguments[0],true),
+		      fdjtSet(arguments[1],true));
+  else {
+    var result=fdjtSet(arguments[0],true);
+    var i=1; while (i<arguments.length) {
+      result=fdjt_union(result,fdjtSet(arguments[i++],true));}
+    return result;}
+}
+
+function fdjtDifference(set1,set2)
+{
+  var results=new Array();
+  var i=0; var j=0; var len1=set1.length; var len2=set2.length;
+  while ((i<len1) && (j<len2))
+    if (set1[i]===set2[j]) {
+      i++; j++;}
+    else if (_fdjt_set_sortfn(set1[i],set2[j])<0)
+      results.push(set1[i++]);
+    else j++;
+  while (i<len1) results.push(set1[i++]);
+  return results;
+}
+
+// This could be faster, but we can do that later
+function fdjtOverlaps(set1,set2)
+{
+  var i=0; var j=0; var len1=set1.length; var len2=set2.length;
+  while ((i<len1) && (j<len2))
+    if (set1[i]===set2[j]) return true;
+    else if (_fdjt_set_sortfn(set1[i],set2[j])<0) i++;
+    else j++;
+  return false;
 }
