f7ba9ae2637e519c64bf74047df309b659f68858
haase <haase@6d381ed0-77be-451f-88c9-2c4333ae1e5d>
2009-11-03 19:16:10 +0000

    Fix AJAX form submission to work in FF with synchronous calls and added synchronicity configurability

--- ajax.js
+++ ajax.js
@@ -128,34 +128,57 @@ function fdjtAjaxSubmit(form)
   if (!(ajax_uri)) return false;
   if ((bridge)&&(bridge!==window)) 
     try {
+      if (fdjt_trace_ajax) fdjtLog("Bridge AJAX submit to %s for %o through %o",ajax_uri,form,bridge);
       return bridge.fdjtAjaxSubmit(form);}
     catch (ex) {
       fdjtLog("Bridge call yielded %o",ex);
       return false;}
   var callback=false;
+  // Whether to do AJAX synchronously or not.
+  var syncp=(form.synchronous)||fdjtCacheAttrib(form,"synchronous");
   if (form.oncallback) callback=form.oncallback;
   else if (form.getAttribute("ONCALLBACK")) {
     callback=new Function
       ("req","form",input_elt.getAttribute("ONCALLBACK"));
     form.oncallback=callback;}
-  var success=false;
+  if (fdjt_trace_ajax)
+    fdjtLog("Direct %s AJAX submit to %s for %o with callback %o",
+	    ((syncp)?("synchronous"):("asynchronous")),
+	    ajax_uri,form,callback);
+  // Firefox doesn't run the callback on synchronous calls
+  var success=false; var callback_run=false;
   var req=new XMLHttpRequest();
   var params=fdjtFormParams(form);
   fdjtAddClass(form,"submitting");
+  if (form.method==="GET")
+    req.open('GET',ajax_uri+"?"+params,(!(syncp)));
+  else req.open('POST',ajax_uri,(!(syncp)));
   req.onreadystatechange=function () {
+    callback_run=true;
+    if (fdjt_trace_ajax)
+      fdjtLog("Got callback (%d,%d) %o for %o through %o, calling %o",
+	      req.readyState,req.status,req,ajax_uri,bridge,callback);
     if ((req.readyState === 4) && (req.status>=200) && (req.status<300)) {
       fdjtDropClass(form,"submitting");
       success=true;
       callback(req,form);}};
-  if (form.method==="GET")
-    req.open('GET',ajax_uri+"?"+params,false);
-  else req.open('POST',ajax_uri,false);
-  if (form.method==="GET") req.send();
-  else {
-    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // req.setRequestHeader("Content-length", params.length);
-    // req.setRequestHeader("Connection", "close");
-    req.send(params);}
+  try {
+    if (form.method==="GET") req.send();
+    else {
+      req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      // req.setRequestHeader("Content-length", params.length);
+      // req.setRequestHeader("Connection", "close");
+      req.send(params);}
+    success=true;}
+  catch (ex) {}
+  if ((syncp) && (!(callback_run))) {
+    if (fdjt_trace_ajax)
+      fdjtLog("Running callback (%d,%d) %o for %o through %o, calling %o",
+	      req.readyState,req.status,req,ajax_uri,bridge,callback);
+    if ((req.readyState === 4) && (req.status>=200) && (req.status<300)) {
+      fdjtDropClass(form,"submitting");
+      success=true;
+      callback(req,form);}};
   return success;
 }
 
