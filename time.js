2b06d0f5a134e0e721ced8633aaf346392df431c
haase <haase@6d381ed0-77be-451f-88c9-2c4333ae1e5d>
2010-08-11 19:37:09 +0000

    Added shortstring time function

--- time.js
+++ time.js
@@ -23,122 +23,137 @@
 /* Time functions */
 
 var fdjtTime=
-  (function (){
-    function fdjtTime() {
-      return (new Date()).getTime();}
-    fdjtTime.revid="$Id$";
-    fdjtTime.version=parseInt("$Revision$".slice(10,-1));
+    (function (){
+	function fdjtTime() {
+	    return (new Date()).getTime();}
+	fdjtTime.revid="$Id$";
+	fdjtTime.version=parseInt("$Revision$".slice(10,-1));
 
-    var loaded=fdjtTime.loaded=(new Date()).getTime();
-    fdjtTime.tick=function(){
-      return (new Date()).getTime()/1000;};
+	var loaded=fdjtTime.loaded=(new Date()).getTime();
+	fdjtTime.tick=function(){
+	    return (new Date()).getTime()/1000;};
 
-    fdjtTime.dateString=function(tstamp){
-      return tstamp.toDateString();};
-    fdjtTime.tick2string=function(tick){
-      return (new Date(tick*1000)).toString();};
-    fdjtTime.tick2date=function(tick){
-      return (new Date(tick*1000)).toDateString();};
-    fdjtTime.tick2locale=function(tick){
-      return (new Date(tick*1000)).toLocaleString();};
-    fdjtTime.tick2time=function(tick){
-      return (new Date(tick*1000)).toTimeString();};
+	fdjtTime.dateString=function(tstamp){
+	    return tstamp.toDateString();};
+	function shortString(tstamp){
+	    var now=new Date();
+	    var diff=(now.getTime()-tstamp.getTime())/1000;
+	    if (diff>(12*3600))
+		return tstamp.toDateString();
+	    else {
+		var hours=tstamp.getHours();
+		var minutes=tstamp.getMinutes();
+		return tstamp.toDateString()+" ("+
+		    ((hours<10)?"0":"")+hours+":"+
+		    ((minutes<10)?"0":"");}}
+	fdjtTime.shortString=shortString;
+	fdjtTime.tick2shortstring=function(tick){
+	    return shortString(new Date(tick*1000));};
 
-    fdjtTime.secs2string=function(interval){
-      if (interval===1)
-	return _("%1 second",interval);
-      else if (interval<10)
-	return _("%1 seconds",interval);
-      else if (interval<60)
-	return _("~%1 seconds",Math.round(interval/60));
-      else if (interval<120) {
-	var minutes=Math.floor(interval/60);
-	var seconds=Math.round(interval-(minutes*60));
-	if (seconds===1)
-	  return _("one minute, one second");
-	else return _("one minute, %1 seconds",seconds);}
-      else if (interval<3600) {
-	var minutes=Math.floor(interval/60);
-	return _("~%1 minutes",minutes);}
-      else if (interval<(2*3600)) {
-	var hours=Math.floor(interval/3600);
-	var minutes=Math.round((interval-(hours*3600))/60);
-	if (minutes===1)
-	  return _("one hour and one minutes");
-	else return _("one hour, %1 minutes",minutes);}
-      else if (interval<(24*3600)) {
-	var hours=Math.floor(interval/3600);
-	return _("~%1 hours",hours);}
-      else if (interval<(2*24*3600)) {
-	var hours=Math.floor((interval-24*3600)/3600);
-	if (hours===1)
-	  return _("one day and one hour");
-	else return _("one day, %1 hours",hours);}
-      else if (interval<(7*24*3600)) {
-	var days=Math.floor(interval/(24*3600));
-	return _("%1 days",days);}
-      else if (interval<(14*24*3600)) {
-	var days=Math.floor((interval-(7*24*3600))/(24*3600));
-	if (days===1)
-	  return "one week and one day";
-	else return _("one week and %1 days",days);}
-      else {
-	var weeks=Math.floor(interval/(7*24*3600));
-	var days=Math.round((interval-(days*7*24*3600))/(7*24*3600));
-	return _("%1 weeks, %2 days",weeks,days);}};
+	fdjtTime.tick2string=function(tick){
+	    return (new Date(tick*1000)).toString();};
+	fdjtTime.tick2date=function(tick){
+	    return (new Date(tick*1000)).toDateString();};
+	fdjtTime.tick2locale=function(tick){
+	    return (new Date(tick*1000)).toLocaleString();};
+	fdjtTime.tick2time=function(tick){
+	    return (new Date(tick*1000)).toTimeString();};
 
-    fdjtTime.secs2short=function(interval){
-      // This is designed for short intervals
-      if (interval<0.001)
-	return Math.round(interval*1000000)+"us";
-      else if (interval<0.1)
-	return Math.round(interval*1000)+"ms";
-      else if (interval<120)
-	return (Math.round(interval*100)/100)+"s";
-      else {
-	var min=Math.round(interval/60);
-	var secs=Math.round(interval-min*6000)/100;
-	return min+"m"+secs+"s";}};
+	fdjtTime.secs2string=function(interval){
+	    if (interval===1)
+		return _("%1 second",interval);
+	    else if (interval<10)
+		return _("%1 seconds",interval);
+	    else if (interval<60)
+		return _("~%1 seconds",Math.round(interval/60));
+	    else if (interval<120) {
+		var minutes=Math.floor(interval/60);
+		var seconds=Math.round(interval-(minutes*60));
+		if (seconds===1)
+		    return _("one minute, one second");
+		else return _("one minute, %1 seconds",seconds);}
+	    else if (interval<3600) {
+		var minutes=Math.floor(interval/60);
+		return _("~%1 minutes",minutes);}
+	    else if (interval<(2*3600)) {
+		var hours=Math.floor(interval/3600);
+		var minutes=Math.round((interval-(hours*3600))/60);
+		if (minutes===1)
+		    return _("one hour and one minutes");
+		else return _("one hour, %1 minutes",minutes);}
+	    else if (interval<(24*3600)) {
+		var hours=Math.floor(interval/3600);
+		return _("~%1 hours",hours);}
+	    else if (interval<(2*24*3600)) {
+		var hours=Math.floor((interval-24*3600)/3600);
+		if (hours===1)
+		    return _("one day and one hour");
+		else return _("one day, %1 hours",hours);}
+	    else if (interval<(7*24*3600)) {
+		var days=Math.floor(interval/(24*3600));
+		return _("%1 days",days);}
+	    else if (interval<(14*24*3600)) {
+		var days=Math.floor((interval-(7*24*3600))/(24*3600));
+		if (days===1)
+		    return "one week and one day";
+		else return _("one week and %1 days",days);}
+	    else {
+		var weeks=Math.floor(interval/(7*24*3600));
+		var days=Math.round((interval-(days*7*24*3600))/(7*24*3600));
+		return _("%1 weeks, %2 days",weeks,days);}};
 
-    fdjtTime.runTimes=function(pname,start){
-      var point=start; var report="";
-      var i=2; while (i<arguments.length) {
-	var phase=arguments[i++]; var time=arguments[i++];
-	report=report+"; "+phase+": "+
-	  ((time.getTime()-point.getTime())/1000)+"s";
-	point=time;}
-      return pname+" "+((point.getTime()-start.getTime())/1000)+"s"+report;};
+	fdjtTime.secs2short=function(interval){
+	    // This is designed for short intervals
+	    if (interval<0.001)
+		return Math.round(interval*1000000)+"us";
+	    else if (interval<0.1)
+		return Math.round(interval*1000)+"ms";
+	    else if (interval<120)
+		return (Math.round(interval*100)/100)+"s";
+	    else {
+		var min=Math.round(interval/60);
+		var secs=Math.round(interval-min*6000)/100;
+		return min+"m"+secs+"s";}};
 
-    fdjtTime.diffTime=function(time1,time2){
-      if (!(time2)) time2=new Date();
-      var diff=time1.getTime()-time2.getTime();
-      if (diff>0) return diff/1000; else return -(diff/1000);
-    };
+	fdjtTime.runTimes=function(pname,start){
+	    var point=start; var report="";
+	    var i=2; while (i<arguments.length) {
+		var phase=arguments[i++]; var time=arguments[i++];
+		report=report+"; "+phase+": "+
+		    ((time.getTime()-point.getTime())/1000)+"s";
+		point=time;}
+	    return pname+" "+((point.getTime()-start.getTime())/1000)+"s"+report;};
 
-    fdjtTime.ET=function(arg){
-      if (!(arg)) arg=new Date();
-      return (arg.getTime()-loaded)/1000;};
+	fdjtTime.diffTime=function(time1,time2){
+	    if (!(time2)) time2=new Date();
+	    var diff=time1.getTime()-time2.getTime();
+	    if (diff>0) return diff/1000; else return -(diff/1000);
+	};
 
-    function timeslice(fcns,slice,space){
-      if (typeof slice !== 'number') slice=0;
-      if (typeof space !== 'number') space=100;
-      var i=0; var lim=fcns.length;
-      var slicefn=function(){
-	var timelim=fdjtTime()+space;
-	var nextspace=false;
-	while (i<lim) {
-	  var fcn=fcns[i++];
-	  if (!(fcn)) continue;
-	  else if (typeof fcn === 'number') {
-	    nextspace=fcn; break;}
-	  else fcn();
-	  if (fdjtTime()>timelim) break;}
-	if (i<lim) setTimeout(slicefn,nextspace||space);};
-      return slicefn();}
+	fdjtTime.ET=function(arg){
+	    if (!(arg)) arg=new Date();
+	    return (arg.getTime()-loaded)/1000;};
 
-    fdjtTime.timeslice=timeslice;
+	function timeslice(fcns,slice,space){
+	    if (typeof slice !== 'number') slice=0;
+	    if (typeof space !== 'number') space=100;
+	    var i=0; var lim=fcns.length;
+	    var slicefn=function(){
+		var timelim=fdjtTime()+space;
+		var nextspace=false;
+		while (i<lim) {
+		    var fcn=fcns[i++];
+		    if (!(fcn)) continue;
+		    else if (typeof fcn === 'number') {
+			nextspace=fcn; break;}
+		    else fcn();
+		    if (fdjtTime()>timelim) break;}
+		if (i<lim) setTimeout(slicefn,nextspace||space);};
+	    return slicefn();}
 
-    return fdjtTime;})();
+	fdjtTime.timeslice=timeslice;
+
+	return fdjtTime;})();
 
 var fdjtET=fdjtTime.ET;
 
