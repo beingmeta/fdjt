96cbc6ee549b697782b4cf571aeb504db1570c18
Ken Haase <kh@beingmeta.com>
2011-05-30 14:06:29 -0400

    Added some missing handlers if needed

--- header.js
+++ header.js
@@ -2,26 +2,52 @@
 
 /* Copyright (C) 2009-2011 beingmeta, inc.
    This file was created from several component files and is
-    part of the FDJT web toolkit (www.fdjt.org)
+   part of the FDJT web toolkit (www.fdjt.org)
 
    This program comes with absolutely NO WARRANTY, including implied
    warranties of merchantability or fitness for any particular
    purpose.
 
    The copyright notice of the individual files are all prefixed by
-    a copyright notice of the form "Copyright (C) ...".
+   a copyright notice of the form "Copyright (C) ...".
 
-    Use, modification, and redistribution of this program is permitted
-    under either the GNU General Public License (GPL) Version 2 (or
-    any later version) or under the GNU Lesser General Public License
-    (version 3 or later).
+   Use, modification, and redistribution of this program is permitted
+   under either the GNU General Public License (GPL) Version 2 (or
+   any later version) or under the GNU Lesser General Public License
+   (version 3 or later).
 
-    These licenses may be found at www.gnu.org, particularly:
-      http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-      http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+   These licenses may be found at www.gnu.org, particularly:
+   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+   http://www.gnu.org/licenses/lgpl-3.0-standalone.html
 
 */
 
 var fdjt_versions=((fdjt_versions)||(new Array()));
 fdjt_versions.decl=function(name,num){
     if ((!(fdjt_versions[name]))||(fdjt_versions[name]<num)) fdjt_versions[name]=num;};
+
+// Some augmentations
+if (!(Array.prototype.indexOf))
+    Array.prototype.indexOf=function(elt,i){
+	if (!(i)) i=0; var len=this.length;
+	while (i<len) if (this[i]===elt) return i; else i++;
+	return -1;};
+// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/keys
+if(!Object.keys) {
+    Object.keys = function(o){
+	if (o !== Object(o))
+            throw new TypeError('Object.keys called on non-object');
+	var ret=[],p;
+	for(p in o) if(Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
+	return ret;}};
+
+if (!String.prototype.trim) {
+    String.prototype.trim = (function () {
+	var trimLeft  = /^\s+/,
+        trimRight = /\s+$/
+
+	return function () {
+	    return this.replace(trimLeft, "").replace(trimRight, "")
+	}
+    })()};
+
