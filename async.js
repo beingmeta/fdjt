b86b10c2c898f0d6ad2a4fe24d729ad90be899a8
Ken Haase <kh@beingmeta.com>
2015-07-24 09:51:58 -0700

    Fix some jshint complaints

--- async.js
+++ async.js
@@ -155,6 +155,61 @@ fdjt.Async=fdjt.ASync=fdjt.async=
             if (watch_slice<1) watch_slice=vec.length*watch_slice;
             return new Promise(slowmapping);};
 
+        // Returns a function, that, as long as it continues to be invoked, will not
+        // be triggered. The function will be called after it stops being called for
+        // N milliseconds. If `immediate` is passed, trigger the function on the
+        // leading edge, instead of the trailing.
+        function debounce(func, wait, immediate) {
+            var timeout;
+            return function() {
+                var context = this, args = arguments;
+                var later = function() {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                };
+                var callNow = immediate && !timeout;
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+                if (callNow) func.apply(context, args);
+            };
+        }
+        fdjtAsync.debounce=debounce;
+
+        function poll(fn, callback, errback, timeout, interval) {
+            var endTime = Number(new Date()) + (timeout || 2000);
+            interval = interval || 100;
+
+            (function p() {
+                // If the condition is met, we're done! 
+                if(fn()) {
+                    callback();
+                }
+                // If the condition isn't met but the timeout hasn't elapsed, go again
+                else if (Number(new Date()) < endTime) {
+                    setTimeout(p, interval);
+                }
+                // Didn't match and too much time, reject!
+                else {
+                    errback(new Error('timed out for ' + fn + ': ' + arguments));
+                }
+            })();
+        }
+        fdjtAsync.poll=poll;
+
+        function once(fn, context) { 
+            var result;
+
+            return function() { 
+                if(fn) {
+                    result = fn.apply(context || this, arguments);
+                    fn = null;
+                }
+
+                return result;
+            };
+        }
+        fdjtAsync.once=once;
+
         return fdjtAsync;})();
 
 /* Emacs local variables
